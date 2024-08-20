87634758471d5cb46825b21430c57f93d58c76a1
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-08-20 09:42:51 +0200

    formatting the TabButton component

--- src/components/Header/TabButton.jsx
+++ src/components/Header/TabButton.jsx
@@ -1,3 +1,7 @@
-export default function TabButton({children}) {
-  return <li><button>{children}</button></li>
+export default function TabButton({ children }) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
 }
