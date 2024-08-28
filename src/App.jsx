885e51f5fcfd802a8793b079c7658bc95f494e90
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-08-28 10:29:25 +0200

    add isSelected arg. to show the selected button with css dinamic style

--- src/App.jsx
+++ src/App.jsx
@@ -44,12 +44,12 @@ function App() {
         <h2>Time to get started!</h2>
         <section id="examples">
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect("components")}>
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           {tabContent}
         </section>
