acc5c33e24a691f5de4c1365a66fb5526c4e343c
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-02 14:00:33 +0200

    use prittier

--- src/components/Header/TabButton.jsx
+++ src/components/Header/TabButton.jsx
@@ -1,7 +1,9 @@
 export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'active' : ''}  {...props}>{children}</button>
+      <button className={isSelected ? "active" : ""} {...props}>
+        {children}
+      </button>
     </li>
   );
 }
