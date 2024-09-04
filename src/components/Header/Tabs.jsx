978bd0376295190493b1363b264260411e80d3f0
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-04 10:10:16 +0200

    set the Tab component dynamically

--- src/components/Header/Tabs.jsx
+++ src/components/Header/Tabs.jsx
@@ -1,7 +1,8 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonContainer }) {
+  const ButtonsContainer = buttonContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
